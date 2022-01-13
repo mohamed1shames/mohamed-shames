318ea842411ae24a1a22c7076121515b910083c2
mohamed1shames <95628826+mohamed1shames@users.noreply.github.com>
2022-01-13 15:56:02 +0200

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+                         "Hyper Text preprocessor",
+                         "Hyper Text Markup Language",
+                         "Hyper Text Multiple Language",
+                         "Hyper Tool multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "what does CSS stand for?",
+        answer: "Cascading style sheet",
+        options: [
+                         "Common style sheet",
+                         "Colorful style sheet",
+                         "Compuuter style sheet",
+                         "Cascading style sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "what does PHP stand for?",
+        answer: "Hypertext preprocessor",
+        options: [
+                         "Hypertext preprocessor",
+                         "Hypertext programming",
+                         "Hypertext preprogramming",
+                         "Hometext preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "what does SQL stand for?",
+        answer: "structured Query Language",
+        options: [
+                         "Stylish Question Language",
+                         "Stylesheet Query Language",
+                         "Statemeant Question Language",
+                         "structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "what does XML stand for?",
+        answer: "extensible Markup Language",
+        options: [
+                         "extensible Markup Language",
+                         "executable Multiple Language",
+                         "exeTra Multi-program Language",
+                         "eXemine Multiple Language"
+        ]
+    },
+    
+];
